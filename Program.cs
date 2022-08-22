d60b7d78632f69096b8889a25fc02dc7dbcc0fb3
asaga <asaga@SWH-ANDREY>
2022-08-23 01:07:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HW2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetInput getInput = new GetInput();
+            double number = getInput.Double("INPUT: ");
+            Console.WriteLine(number);
+            Console.ReadKey();
+        }
+    }
+    internal class GetInput
+    {
+        internal double Double(string _askDouble)
+        {
+            string _str = string.Empty;
+            char c;
+            ConsoleKeyInfo cki = new ConsoleKeyInfo();
+            char separateChar = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0];
+            if (Console.CursorLeft > 0) _askDouble = "\n" + _askDouble;
+            Console.Write(_askDouble);
+            while (true)
+            {
+                cki = Console.ReadKey(true);
+                if (cki.Key == ConsoleKey.Backspace && _str.Length > 0)
+                {
+                    _str = _str.Remove(_str.Length - 1);
+                    Console.Write("\b \b");
+                    continue;
+                }
+                c = cki.KeyChar;
+                if (c == '.' || c == ',') c = separateChar;
+                _str += c.ToString();
+                if (double.TryParse(_str, out double result))
+                    Console.Write(c);
+                else
+                    _str = _str.Remove(_str.Length - 1);
+                if (cki.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    return result;
+                }
+            }
+        }
+    }
+}
